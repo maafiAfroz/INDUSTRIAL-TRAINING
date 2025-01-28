7daf9d869c351e4001151f9f38403c05234d94ad
Maafi Afroz <mahmudaafroz109@gmail.com>
2025-01-28 20:08:30 +0600

    fibonacci number

--- test.cpp
+++ test.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        std::cout << i << " ";
+    int n = 10; // Change this to print more Fibonacci numbers
+    int first = 0, second = 1, next;
+
+    std::cout << "Fibonacci Series: " << first << " " << second << " ";
+
+    for (int i = 2; i < n; i++) {
+        next = first + second;
+        std::cout << next << " ";
+        first = second;
+        second = next;
     }
+
     std::cout << std::endl;
     return 0;
 }
